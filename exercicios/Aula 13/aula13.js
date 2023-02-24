2b3153268c8e5d646c34df8ed3dfe5e3e3f89fec
PauloCesardev99 <122824870+PauloCesardev99@users.noreply.github.com>
2023-02-24 00:00:43 -0300

    novos exercicios feitos

---
+++ exercicios/Aula 13/aula13.js
@@ -0,0 +1,7 @@
+var c = 1
+
+while (c<=5){
+    console.log ('Paulo')
+ c =c+1
+
+}
---
+++ exercicios/ex003/ex.js
@@ -0,0 +1,9 @@
+var c = 0
+var fim = 12
+var passo = 2
+
+for(var c=5; c<=12; c= c+passo ){
+
+    console.log(c)
+
+}
